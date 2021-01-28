878b3c15d0ae8b8d1aa6f75b617586d4633f9363
Akshay <73235779+akshay-aws@users.noreply.github.com>
2021-01-28 00:34:00 -0500

    Helper files for AWS::S3Outposts::Endpoint resource (#24)
    This commit captures all the helper files which will be used by the Endpoint resource handlers.

---
+++ aws-s3outposts-endpoint/src/main/java/software/amazon/s3outposts/endpoint/Configuration.java
@@ -0,0 +1,8 @@
+package software.amazon.s3outposts.endpoint;
+
+class Configuration extends BaseConfiguration {
+
+    public Configuration() {
+        super("aws-s3outposts-endpoint.json");
+    }
+}
