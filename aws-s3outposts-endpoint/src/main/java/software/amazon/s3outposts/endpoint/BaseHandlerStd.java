32954c749d46da8e5869b4031dfa99b4c1ab711f
Akshay <73235779+akshay-aws@users.noreply.github.com>
2021-01-31 18:09:48 -0500

    Adding ReadHandler support for Endpoint resource (#28)
    CFN requires every resource to have a ReadHandler as per SIMT V310765594.  
    
    Since the S3Outposts service doesn't provide a `GetEndpoint` call, we will call `ListEndpoints` and filter the
    response using the Arn provided by the caller in the Read request model.
    
    The code has been modified to support this new behavior. Added unit-tests for ReadHandler.
    
    Successfully built the package using `mvn clean package`.

--- aws-s3outposts-endpoint/src/main/java/software/amazon/s3outposts/endpoint/BaseHandlerStd.java
+++ aws-s3outposts-endpoint/src/main/java/software/amazon/s3outposts/endpoint/BaseHandlerStd.java
@@ -6,11 +6,14 @@ import software.amazon.awssdk.services.s3outposts.model.S3OutpostsException;
 import software.amazon.awssdk.services.s3outposts.model.S3OutpostsRequest;
 import software.amazon.cloudformation.proxy.*;
 
+import java.util.stream.Collectors;
+
 public abstract class BaseHandlerStd extends BaseHandler<CallbackContext> {
 
     // Error messages (returned to the customer)
     protected static final String INVALID_INPUT = "OutpostId, SecurityGroupId, SubnetId are required parameters.";
     protected static final String ENDPOINT_ARN_REQD = "Endpoint ARN is required.";
+    protected static final String ENDPOINT_ARN_NOT_FOUND = "Endpoint with provided ARN not found.";
 
     @Override
     public final ProgressEvent<ResourceModel, CallbackContext> handleRequest(
@@ -81,5 +84,62 @@ public abstract class BaseHandlerStd extends BaseHandler<CallbackContext> {
 
     }
 
+    /**
+     * Calls the API listEndpoints.
+     * Ref: https://sdk.amazonaws.com/java/api/latest/software/amazon/awssdk/services/s3outposts/model/ListEndpointsRequest.html
+     * Gets called from the ListHandler and the ReadHandler.
+     */
+    protected ProgressEvent<ResourceModel, CallbackContext> listEndpoints(
+            AmazonWebServicesClientProxy proxy,
+            ProxyClient<S3OutpostsClient> proxyClient,
+            ResourceHandlerRequest<ResourceModel> request,
+            ProgressEvent<ResourceModel, CallbackContext> progress,
+            Logger logger) {
+
+        ResourceModel model = progress.getResourceModel();
+        CallbackContext callbackContext = progress.getCallbackContext();
+
+        if (model != null && model.getArn() != null) {
+            // true only when we call `listEndpoints` from the ReadHandler
+            logger.log(String.format("%s::Read::listEndpoints, arn: %s", ResourceModel.TYPE_NAME, model.getArn()));
+        }
+
+        if (request.getNextToken() != null) {
+            logger.log(String.format("%s::List::listEndpoints - Account: %s, NextToken: %s",
+                    ResourceModel.TYPE_NAME, request.getAwsAccountId(), request.getNextToken()));
+        } else {
+            logger.log(String.format("%s::Read/List::listEndpoints - Account: %s",
+                    ResourceModel.TYPE_NAME, request.getAwsAccountId()));
+        }
+
+        return proxy.initiate("AWS-S3Outposts-Endpoint::Read/List::ListEndpoints", proxyClient, model, callbackContext)
+                .translateToServiceRequest(resourceModel ->
+                        Translator.translateToSdkListEndpointsRequest(request.getNextToken())
+                )
+                .makeServiceCall((listEndpointsRequest, s3ControlProxyClient) ->
+                        s3ControlProxyClient.injectCredentialsAndInvokeV2(listEndpointsRequest,
+                                s3ControlProxyClient.client()::listEndpoints)
+                )
+                .handleError(this::handleError)
+                .done((listEndpointsResponse) ->
+                        ProgressEvent.<ResourceModel, CallbackContext>builder()
+                                // We have to keep the model intact in the ProgressEvent object for the sake of the ReadHandler.
+                                // When a user calls the ReadHandler, they provide the Arn in the model, and we don't want to lose this information.
+                                .resourceModel(model)
+                                .resourceModels(
+                                        listEndpointsResponse.endpoints()
+                                                .stream()
+                                                .map(endpoint ->
+                                                        Translator.translateFromSdkEndpoint(endpoint)
+                                                )
+                                                .collect(Collectors.toList())
+                                )
+                                .nextToken(listEndpointsResponse.nextToken())
+                                .status(OperationStatus.IN_PROGRESS)
+                                .build()
+
+                );
+
+    }
 
 }
--- aws-s3outposts-endpoint/src/main/java/software/amazon/s3outposts/endpoint/ListHandler.java
+++ aws-s3outposts-endpoint/src/main/java/software/amazon/s3outposts/endpoint/ListHandler.java
@@ -3,8 +3,6 @@ package software.amazon.s3outposts.endpoint;
 import software.amazon.awssdk.services.s3outposts.S3OutpostsClient;
 import software.amazon.cloudformation.proxy.*;
 
-import java.util.stream.Collectors;
-
 public class ListHandler extends BaseHandlerStd {
 
     private Logger logger;
@@ -20,59 +18,12 @@ public class ListHandler extends BaseHandlerStd {
         final ResourceModel model = request.getDesiredResourceState();
 
         return ProgressEvent.progress(model, callbackContext)
-                .then(progress -> listEndpoints(proxy, proxyClient, request, progress, logger));
+                .then(progress -> listEndpoints(proxy, proxyClient, request, progress, logger))
+                .then(progress -> {
+                    progress.setStatus(OperationStatus.SUCCESS);
+                    return progress;
+                });
 
     }
 
-    /**
-     * Calling the API listEndpoints
-     *
-     * @param proxy
-     * @param proxyClient
-     * @param request
-     * @param progress
-     * @param logger
-     * @return
-     */
-    private ProgressEvent<ResourceModel, CallbackContext> listEndpoints(
-            AmazonWebServicesClientProxy proxy,
-            ProxyClient<S3OutpostsClient> proxyClient,
-            ResourceHandlerRequest<ResourceModel> request,
-            ProgressEvent<ResourceModel, CallbackContext> progress,
-            Logger logger) {
-
-        ResourceModel model = progress.getResourceModel();
-        CallbackContext callbackContext = progress.getCallbackContext();
-
-        if (request.getNextToken() != null) {
-            logger.log(String.format("%s::List::listEndpoints - Account: %s, NextToken: %s",
-                    ResourceModel.TYPE_NAME, request.getAwsAccountId(), request.getNextToken()));
-        } else {
-            logger.log(String.format("%s::List::listEndpoints - Account: %s",
-                    ResourceModel.TYPE_NAME, request.getAwsAccountId()));
-        }
-
-        return proxy.initiate("AWS-S3Outposts-Endpoint::List::ListEndpoints", proxyClient, model, callbackContext)
-                .translateToServiceRequest(resourceModel ->
-                        Translator.translateToSdkListEndpointsRequest(request.getNextToken())
-                )
-                .makeServiceCall((listEndpointsRequest, s3ControlProxyClient) ->
-                        // Ref: https://sdk.amazonaws.com/java/api/latest/software/amazon/awssdk/services/s3outposts/model/ListEndpointsRequest.html
-                        s3ControlProxyClient.injectCredentialsAndInvokeV2(listEndpointsRequest,
-                                s3ControlProxyClient.client()::listEndpoints))
-                .handleError(this::handleError)
-                .done(listEndpointsResponse ->
-                        ProgressEvent.<ResourceModel, CallbackContext>builder()
-                                .resourceModels(
-                                        listEndpointsResponse.endpoints()
-                                                .stream()
-                                                .map(endpoint -> Translator.translateFromSdkEndpoint(endpoint))
-                                                .collect(Collectors.toList())
-                                )
-                                .nextToken(listEndpointsResponse.nextToken())
-                                .status(OperationStatus.SUCCESS)
-                                .build());
-
-
-    }
 }
