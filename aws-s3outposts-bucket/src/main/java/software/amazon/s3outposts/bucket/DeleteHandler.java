2a94f79dd13c233c651e79e2ca64eb37db17f284
Akshay <nadaksha@amazon.com>
2021-02-06 14:55:54 -0500

    Adding retry logic to Bucket DeleteHandler
    The service returns a 409 InvalidBucketState error when we try to Delete a bucket which is not
    in the 'Active' state. This results in DeleteBucket failing in CFN if issued immediately after
    a CreateBucket call. This also happens when the Create operation fails for other reasons when
    done through CFN and CFN automatically tries to delete the bucket and rollback the stack. At such
    times the DeleteBucket call fails since the Bucket was not in the correct state.
    
    The fix is to check the error code and if its a 409 InvalidBucketState, we will keep issuing the
    DeleteBucket call until it succeeds.
    
    Further enhancements can be made - such as adding a limited number of retries - in case we see
    situations where the Delete Retry operation from CFN never ends.
    
    Successfully build using `mvn clean package`.

--- aws-s3outposts-bucket/src/main/java/software/amazon/s3outposts/bucket/DeleteHandler.java
+++ aws-s3outposts-bucket/src/main/java/software/amazon/s3outposts/bucket/DeleteHandler.java
@@ -2,6 +2,7 @@ package software.amazon.s3outposts.bucket;
 
 import com.amazonaws.util.StringUtils;
 import software.amazon.awssdk.services.s3control.S3ControlClient;
+import software.amazon.awssdk.services.s3control.model.S3ControlException;
 import software.amazon.cloudformation.proxy.*;
 
 public class DeleteHandler extends BaseHandlerStd {
@@ -23,8 +24,28 @@ public class DeleteHandler extends BaseHandlerStd {
             return ProgressEvent.failed(model, callbackContext, HandlerErrorCode.InvalidRequest, BUCKET_ARN_REQD);
         }
 
-        // Initiate the callGraph and get the callContext
-        return proxy.initiate("AWS-S3Outposts-Bucket::Delete", proxyClient, model, callbackContext)
+        return ProgressEvent.progress(model, callbackContext)
+                .then(progress -> deleteBucket(proxy, proxyClient, request, progress, logger));
+
+    }
+
+    /**
+     * Calls the API deleteBucket
+     */
+    private ProgressEvent<ResourceModel, CallbackContext> deleteBucket(
+            final AmazonWebServicesClientProxy proxy,
+            final ProxyClient<S3ControlClient> proxyClient,
+            final ResourceHandlerRequest<ResourceModel> request,
+            final ProgressEvent<ResourceModel, CallbackContext> progressEvent,
+            final Logger logger
+    ) {
+
+        final ResourceModel model = progressEvent.getResourceModel();
+        final CallbackContext context = progressEvent.getCallbackContext();
+
+        logger.log(String.format("%s::Delete::deleteBucket called for arn: %s \n", ResourceModel.TYPE_NAME, model.getArn()));
+
+        return proxy.initiate("AWS-S3Outposts-Bucket::Delete", proxyClient, model, context)
                 // Form DeleteBucketRequest
                 .translateToServiceRequest(resourceModel -> Translator.translateToDeleteRequest(resourceModel, request.getAwsAccountId()))
                 // Issue call deleteBucket
@@ -32,7 +53,13 @@ public class DeleteHandler extends BaseHandlerStd {
                         s3ControlProxyClient.injectCredentialsAndInvokeV2(deleteBucketRequest, s3ControlProxyClient.client()::deleteBucket)
                 )
                 .handleError((deleteBucketRequest, exception, s3ControlProxyClient, resourceModel, cbContext) -> {
-                    logger.log(String.format("%s - DeleteHandler - Error type: %s", ResourceModel.TYPE_NAME, exception.getClass().getCanonicalName()));
+                    if (exception instanceof S3ControlException && ((S3ControlException) exception).statusCode() == 409 &&
+                            ((S3ControlException) exception).awsErrorDetails().errorCode().equals(INVALID_BUCKET_STATE)) {
+                        logger.log(String.format("%s::Delete::handleRequest - Error Msg: %s",
+                                ResourceModel.TYPE_NAME, ((S3ControlException) exception).awsErrorDetails().errorMessage()));
+                        return ProgressEvent.defaultInProgressHandler(cbContext, 20, resourceModel);
+                    }
+                    logger.log(String.format("%s::Delete::handleRequest - Error type: %s", ResourceModel.TYPE_NAME, exception.getClass().getCanonicalName()));
                     return handleError(deleteBucketRequest, exception, s3ControlProxyClient, resourceModel, cbContext);
                 })
                 .done(deleteBucketResponse -> ProgressEvent.defaultSuccessHandler(null));
